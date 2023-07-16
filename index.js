4abeeb62887122a70331ddd77683a0e4ce32a887
DEEPAK KUMAR <deepakkumar@DEEPAKs-MacBook-Pro.local>
2023-07-17 01:06:08 +0530

    added search

--- index.js
+++ index.js
@@ -41,12 +41,12 @@ const allowedOrigins = [
     next();
   });
 
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.sellyourcamera.in');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   res.setHeader('Access-Control-Allow-Origin', 'https://www.sellyourcamera.in');
+  //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  //   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  //   next();
+  // });
   
 
   //reset cookies
@@ -126,5 +126,5 @@ app.use('/api',userCallRequestRoute);
 //to fetch instagram feeds
 app.use('/api',instaRoute);
 //to search product
-app.use('/api',search_product_route);
+// app.use('/api',search_product_route);
 
