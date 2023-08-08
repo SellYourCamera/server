eab5e179792c37499be9864840c999a169e0aacf
DEEPAK KUMAR <deepakkumar@DEEPAKs-MacBook-Pro.local>
2023-08-08 06:50:59 +0530

    changes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const lensRoute = require("./routes/lensRoute");
 const userCallRequestRoute =require("./routes/userCallRequestRoute");
 const serachProductsRoute = require("./routes/searchProductsRoute");
 const axios = require('axios');
-// const search_product_route = require("./routes/searchProductsRoute");
+ const search_product_route = require("./routes/searchProductsRoute");
 
 
 // Enable CORS for all routes
@@ -126,5 +126,5 @@ app.use('/api',userCallRequestRoute);
 //to fetch instagram feeds
 app.use('/api',instaRoute);
 //to search product
-// app.use('/api',search_product_route);
+ app.use('/api',search_product_route);
 
